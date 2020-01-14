dedd928b81486043fefe075f29751a2a31bab9e4
MrMerluso <59891718+MrMerluso@users.noreply.github.com>
2020-01-14 16:22:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var qrcode = new QRCode(document.getElementById("qrcode"), {
+	width : 1000,
+	height : 1000
+});
+
+function makeCode () {		
+	var elText = document.getElementById("text");
+	
+	if (!elText.value) {
+		alert("Input a text");
+		elText.focus();
+		return;
+	}
+	
+	qrcode.makeCode(elText.value);
+}
+
+
+
+$("#text").
+	on("blur", function () {
+		makeCode();
+	}).
+	on("keydown", function (e) {
+		if (e.keyCode == 13) {
+			makeCode();
+		}
+	});
