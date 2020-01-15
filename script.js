e0f17764fb24846049d0935dfbd8ab0a278be815
MrMerluso <59891718+MrMerluso@users.noreply.github.com>
2020-01-14 21:54:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-var form = document.getElementById("form");
-form.onsubmit = function(e) {
-	e.preventDefault();
-	var name = document.getElementById("nomb");
-	var ape = document.getElementById("ap");
-	var rut = document.getElementById("rut");
-	var ver = document.getElementById("ver");
-	var depto = document.getElementById("depto");
-	var qrcode = new QRCode(document.getElementById("qrcode"), name.value + ape.value + rut.value + ver.value + depto.value)
-}
+var nombre = document.getElementById("nom");
+var apellido = document.getElementById("ap");
+var rut = document.getElementById("rut");
+var veri = document.getElementById("ver");
+var depto = document.getElementById("depto");
+var qrdata = nombre.concat(apellido, rut, veri, depto);
+var qrcode = new QRCode(document.getElementById("qrcode"));
 
+function generateQR(){
+	var data = qrdata.value;
+	qrcode.makeCode(qrdata)
+}
